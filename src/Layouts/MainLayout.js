0d5ebab00a7955c1bed13716a59bafde1397ef37
seohee <jsh001505@gmail.com>
2021-11-02 18:50:04 +0900

    navbar 마우스 이벤트 등록

---
+++ src/Layouts/MainLayout.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { makeStyles } from '@mui/styles';
+import { Typography } from '@mui/material';
+import '../App.css';
+import Navbar from '../Layouts/Navbar';
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+  logo: {
+    paddingLeft: '20%',
+    paddingTop: '4%',
+    paddingBottom: '2%',
+  },
+  logoText: {
+    color: '#0b0645',
+    fontWeight: 'bold',
+  },
+});
+
+export default function MainLayout() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <div className={classes.logo}>
+        <Typography className={classes.logoText} variant='h3'>
+          D'Food
+        </Typography>
+      </div>
+      <Navbar />
+    </div>
+  );
+}
